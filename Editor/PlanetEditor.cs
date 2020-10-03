ddad12241c6c155ba54103c22ebc064a22bc79df
MalCaor <xav.lemen@orange.fr>
2020-10-03 18:48:35 +0200

    separate editor

--- Editor/PlanetEditor.cs
+++ Editor/PlanetEditor.cs
@@ -21,6 +21,7 @@ public class PlanetEditor : Editor
     {
         using(var check = new EditorGUI.ChangeCheckScope())
         {
+            EditorGUILayout.InspectorTitlebar(true, settings);
             Editor editor = CreateEditor(settings);
             editor.OnInspectorGUI();
 
